02a3f64f2288abe4db58180da594dfea989491e5
Luci <georgeskotidismain@gmail.com>
2024-05-27 00:30:49 +0300

    testing something

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
-app.set('announcements', path.join(__dirname,'views/announcements'));
 
 app.get('/', (req,res) => res.render('index'));
 
